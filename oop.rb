6c45fabb075d68079c623dec648a0872ff5d16f8
Sarah Garlock <garlock.sarah10@gmail.com>
2023-02-11 23:30:07 -0700

    Add oop.rb and self eval

--- oop.rb
+++ oop.rb
@@ -1,16 +1,56 @@
 # Write a class called Unicorn
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
-# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+# it should have a method called "say" that returns whatever
+# string is passed in, with "*~*" at the beginning and end of the string
+
+class Unicorn
+    def initialize(name)
+        @name = name
+        @color = "Silver"
+    end
+    
+    def say
+        puts "*~* This is a unicorn *~*"
+    end
+end
+
+unicorn1 = Unicorn.new("Fred")
+p unicorn1.say
 
 
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
-#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
+#  it should have a pet attribute, that is a bat, by default BUT 
+#  it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    def initialize(name)
+        @name = name
+        @pet = "bat"
+        @thirsty = true
+        @drink = true
+    end
+
+    def change_drink(new_drink)
+        @drink = new_drink
+    end
+
+    def change_pet(new_pet)
+        @pet = new_pet
+    end
+
+end
+
+vampire1 = Vampire.new("Fred")
+p vampire1
+vampire1.change_drink(false)
+p vampire1
+vampire1.change_pet("Mouse")
+p vampire1
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -19,6 +59,24 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    def initialize(name, rider,color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+        @eat = "I'm hungry"
+    end
+  
+    def change_eat(new_eat)
+      @eat = new_eat
+    end
+  end
+  
+  dragon1 = Dragon.new("Toothless", "John", "Green")
+  p dragon1
+  p dragon1
+  p dragon1
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +86,40 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit   
+    def initialize(name, disposition, age)
+        @name = name
+        @disposition = disposition
+        @age = age
+        @is_adult = false
+        @is_old = false
+        @has_rings = false
+    end
+     
+    def celebrate_birthday(new_age)
+        @age = new_age + 1
+    end
+
+    def adult(new_adult)
+        if @age > 33
+            new_adult == true
+        end
+    end
+
+
+    def has_ring(ring)
+        if has_ring == true
+            puts "Frodo"
+        end
+    end
+
+end
+hobbit1 = Hobbit.new("Bippy", "sunny", 43)
+p hobbit1
+hobbit1.celebrate_birthday(35)
+p hobbit1
+hobbit1.adult(100)
+p hobbit1
+ 
+# I honestly am very confused by this example, here is my best shot.
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self-Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+Pretty tough, not easy to get through and I did not answer everything 100%. I can see the concepts and talking out loud helps but not everything is clicking yet. Hopefully with more practice and instruction this will become easier
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+Going alright, I have found myself working 5-7 hours of school on top of a full time job so I'm very tired. I have been good about taking breaks and jumping right back into the work. 
 - What did you learn from this exercise that you want to remember as you begin Mod 1?
+Read through everything several times before starting and make sure I understand everything as well as I can. Save all your good resources!
